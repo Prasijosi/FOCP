5199a636eba3e3f8d4a643d9e34640204cd5bcc2
Prasijosi <jprashiddha5@gmail.com>
2023-11-14 10:41:44 +0545

    Programs 02

--- W2/Prashiddha_Joshi_P2.py
+++ W2/Prashiddha_Joshi_P2.py
@@ -1,13 +1,13 @@
-# Qn no 1
+#1
 user=input("Hello what is your name? ")
 print(f"Hello {user}. Nice to meet you." )
 
-# Qn no 2
+#2
 celsius = int(input("Enter temperature in celsius: "))
 fahrenheit = (celsius * 9/5) + 32
 print(f"{celsius}.C is equivalent to {fahrenheit}.F")
 
-# Qn no 3
+#3
 Students = int(input("Number of Students : "))
 req_size = int(input("Required group size :  "))
 num_of_group = Students // req_size
@@ -23,7 +23,7 @@ if rem_students == 0:
 else:
     print(f"There will be {num_of_group} {group_word} with {rem_students} student{'s' if rem_students > 1 else ''} left.")
 
-# Qn no 4
+# 4
 tub_of_sweets = int(input("Enter total number of sweets: "))
 pupils = int(input("Ente total number of pupils: "))
 sweet_pupils = tub_of_sweets // pupils
