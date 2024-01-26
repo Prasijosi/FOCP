9918a555df30540ba3b3b980ef6dcc52699a9581
Prasijosi <jprashiddha5@gmail.com>
2024-01-26 12:06:56 +0545

    FINAL ASSIGNMENTS

---
+++ Final Project/Task 3/login.py
@@ -0,0 +1,21 @@
+import os
+import main
+
+def login(username, password):
+    # Check if the username exists before prompting for a password
+    info_user = main.retrieve_user_data(username)
+    if not info_user:
+        print("Error:Invalid user id or password.")
+        return False
+
+    # If username exists, proceed with password verification
+    if main.password_encryption(password) == info_user[1]:
+        return True
+
+username = input("Enter username:").lower()
+password = input("Enter a password:")
+
+if login(username, password):
+        print("Welcome!!")
+else:
+        print("Incorrect password\n")
